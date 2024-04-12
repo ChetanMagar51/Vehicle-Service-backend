6eafdc9316ef7d8ab22b501839168067c3e85837
chetan magar <sonumagar7264@gmail.com>
2024-04-12 17:40:52 +0530

    Minor changes done

--- src/test/java/com/application/vehicleservicemanagement/controller/AuthenticationControllerTest.java
+++ src/test/java/com/application/vehicleservicemanagement/controller/AuthenticationControllerTest.java
@@ -5,13 +5,13 @@ import com.application.vehicleservicemanagement.service.AuthenticationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
